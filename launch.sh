e172f41036f8daca020bdf96a974b108cbc93e84
sina021 <sina021370@gmail.com>
2016-05-19 14:41:21 +0430

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -33,15 +33,16 @@ install_luarocks() {
 }
 
 install_rocks() {
-  ./.luarocks/bin/luarocks install luasocket
+  ./.luarocks/bin/luarocks install luasec
   RET=$?; if [ $RET -ne 0 ];
     then echo "Error. Exiting."; exit $RET;
   fi
 
-  ./.luarocks/bin/luarocks install oauth
+  ./.luarocks/bin/luarocks install lbase64 20120807-3
   RET=$?; if [ $RET -ne 0 ];
     then echo "Error. Exiting."; exit $RET;
   fi
+
 
   ./.luarocks/bin/luarocks install redis-lua
   RET=$?; if [ $RET -ne 0 ];
@@ -110,6 +111,15 @@ else
     echo "Run $0 install"
     exit 1
   fi
+  
+  #Adding some color. By @iicc1 :D
+   echo -e "\033[38;5;208m"
+   echo -e "      ____  ____ _____                        "
+   echo -e "     |    \|  _ )_   _|___ ____   __  __      "
+   echo -e "     | |_  )  _ \ | |/ .__|  _ \_|  \/  |     "
+   echo -e "     |____/|____/ |_|\____/\_____|_/\/\_|     "
+   echo -e "                                              \033[0;00m"
+   echo -e "\e[36m"
 
   ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/bot.lua -l 1 -E $@
 fi
